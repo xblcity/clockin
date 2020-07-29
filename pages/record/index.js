93b6d8db3fb80d051dfc0354330dbede6e626d5b
xblcity <hyaxie@163.com>
2020-07-29 22:59:51 +0800

    feat: 编辑页面内容

--- pages/record/index.js
+++ pages/record/index.js
@@ -6,10 +6,10 @@ Page({
     logs: []
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+    // this.setData({
+    //   logs: (wx.getStorageSync('logs') || []).map(log => {
+    //     return util.formatTime(new Date(log))
+    //   })
+    // })
   }
 })
