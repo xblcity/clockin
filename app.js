804c3c25cec2efce14eee90bab1a529024fff66c
xblcity <hyaxie@163.com>
2020-08-05 21:45:53 +0800

    feat: 打卡界面

--- app.js
+++ app.js
@@ -4,46 +4,49 @@ App({
     // 小程序启动
     // 登录
     wx.login({
-      success: res => {
+      success: (res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res.code)
+        console.log(res.code);
         wx.request({
           url: `${this.globalData.host}/wxLogin`,
           data: {
-            code: res.code
+            code: res.code,
           },
-          success (res) {
-            console.log(res.data)
-            const { userId } = res.Date
+          success(res) {
+            console.log(res.data);
+            const { userId } = res.Date;
             // 存储用户id
-            wx.setStorageSync('userId', userId)
-          }
-        })
-      }
-    })
+            wx.setStorageSync("userId", userId);
+          },
+          fail() {
+            console.error("请求失败");
+          },
+        });
+      },
+    });
     // 获取用户信息
     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
+      success: (res) => {
+        if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
-            success: res => {
+            success: (res) => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(res);
               }
-            }
-          })
+            },
+          });
         }
-      }
-    })
+      },
+    });
   },
   globalData: {
     userInfo: null,
-    host: 'localhost:3001/api'
-  }
-})
+    host: "localhost:3001/api",
+  },
+});
--- pages/clockin/index.js
+++ pages/clockin/index.js
@@ -1,59 +1,109 @@
 //index.js
 //获取应用实例
 const app = getApp();
+import moment from '../../utils/moment.min.js'
 
 Page({
   data: {
-    dialogShow: false,
-    oneButton: [{ text: "确定" }],
+    successShow: false,
+    sucessText: "",
+    buttonText: [{ text: "确定" }],
     errShow: false,
-    errText: ''
+    errText: "",
+    userId: "",
   },
+  // 隐藏dialog
   tapDialogButton(e) {
     this.setData({
       dialogShow: false,
     });
   },
+  // 点击起床打卡
   tapWakeUpButton(e) {
-    const hour = new Date().getHours()
+    const hour = new Date().getHours();
     if (hour > 12) {
       this.setData({
         errShow: true,
-        errText: '打卡不在时间统计范围内'
-      })
+        errText: "打卡不在时间0~12点范围内",
+      });
+      return;
     }
-    const userId = wx.getStorageSync('userId')
+    const userId = wx.getStorageSync("userId");
     // 发起请求
+    console.log(moment())
+    const that = this;
     wx.request({
-      url: `${this.globalData.host}/postTime`,
+      url: `${app.globalData.host}/postTime`,
       data: {
         userId,
         dateTime,
-        wakeUpTime,
-        bedTime
+        bedTime,
       },
-      success (res) {
-        console.log(res.data)
-      }
-    })
-    // this.setData({
-    //   showOneButtonDialog: true,
-    // });
+      success(res) {
+        if (res.statusCode !== 200) {
+          that.setData({
+            errShow: true,
+            errText: res.data.errMsg,
+          });
+          return
+        }
+        that.setData({
+          successShow: true,
+          sucessText: res.data.successMsg,
+        });
+      },
+      fail() {
+        that.setData({
+          errShow: true,
+          errText: "请求出错",
+        });
+      },
+    });
   },
   tapBedButton(e) {
-    const hour = new Date().getHours()
+    const hour = new Date().getHours();
     if (hour < 20) {
       this.setData({
         errShow: true,
-        errText: '打卡不在时间统计范围内'
-      })
+        errText: "打卡不在时间8~12点范围内",
+      });
+      return;
     }
-    // this.setData({
-    //   showOneButtonDialog: true,
-    // });
+    const that = this;
+    const dateTime = moment().format('YYYY-MM-DD')
+    const bedTime = moment().format('HH:mm:ss')
+    wx.request({
+      url: `${app.globalData.host}/postTime`,
+      data: {
+        // userId,
+        dateTime,
+        bedTime,
+      },
+      success(res) {
+        if (res.statusCode !== 200) {
+          that.setData({
+            errShow: true,
+            errText: res.data.errMsg,
+          });
+          return
+        }
+        that.setData({
+          successShow: true,
+          sucessText: res.data.successMsg,
+        });
+      },
+      fail() {
+        that.setData({
+          errShow: true,
+          errText: "请求出错",
+        });
+      },
+    });
   },
   onLoad: function () {
-    const userId = wx.getStorageSync('userId')
-
+    const userId = wx.getStorageSync("userId");
+    this.setData({
+      userId,
+    });
   },
 });
