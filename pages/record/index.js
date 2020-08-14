acde59406e0b4beb6d6689a0e714840fd79015dd
huya <huya@tongyu.tech>
2020-08-14 18:04:45 +0800

    feat: 数据处理

--- pages/record/index.js
+++ pages/record/index.js
@@ -2,18 +2,37 @@ import * as echarts from "../../ec-canvas/echarts";
 
 const app = getApp();
 
-function initChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr, // new
-  });
-  canvas.setChart(chart);
-
-  var option = {
+const typeMap = {
+  week: "本周统计",
+  month: "本月统计",
+};
+function setOption(chart, type, data) {
+  let type = "week";
+  let dayTotal;
+  if (type === "week") {
+    dayTotal = 7;
+  }
+  if (type === "month") {
+    dayTotal = parseInt(moment().endOf("month").format("DD"));
+  }
+  const source = [...new Array(dayTotal)]
+    .map((item, index) =>
+      type === "week"
+        ? moment().startOf("week").add(index, "days").format("YYYY-MM-DD")
+        : moment().startOf("month").add(index, "days").format("YYYY-MM-DD")
+    )
+    .map((date) => {
+      const target = data.find((item) => item.dateTime === date);
+      if (target) {
+        const { dateTime, sleepTime, wakeUpTime, bedTime } = target;
+        return [dateTime, sleepTime, wakeUpTime, bedTime];
+      }
+      return [date, "", "", "", ""];
+    });
+  const options = {
     // 标题
     title: {
-      text: "本周统计",
+      text: typeMap[type],
       textStyle: {
         color: "#333",
         fontWeight: "normal",
@@ -49,15 +68,7 @@ function initChart(canvas, width, height, dpr) {
       },
     ],
     dataset: {
-      source: [
-        ["7/12", "6.12", "20.12", 7],
-        ["7/13", "7.12", "20.12", 5],
-        ["7/14", "7.12", "22.12"],
-        ["7/15", "7.12", "", 6],
-        ["7/16", "7.12", "19.12", 4],
-        ["7/17", "7.12", "20.12", 10],
-        ["7/18", "7.12", "21.12", 9],
-      ],
+      source,
     },
     // 单个或者多个组合
     series: [
@@ -113,145 +124,7 @@ function initChart(canvas, width, height, dpr) {
       },
     ],
   };
-
-  chart.setOption(option);
-  return chart;
-}
-
-function initMonthChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr, // new
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    // 标题
-    title: {
-      text: "本月统计",
-      textStyle: {
-        color: "#333",
-        fontWeight: "normal",
-        fontSize: "16px",
-      },
-      padding: [10, 0, 0, 0],
-      left: "center",
-    },
-    // 提示框
-    tooltip: {
-      formatter: function (params) {
-        const { value } = params;
-        // 时间转换
-        const wakeUpTime = value[1] && value[1].split();
-        const bedTime = value[2] && value[2].replace(".", ":");
-        return `  起床时间：${wakeUpTime}
-  睡觉时间：${bedTime}
-  睡眠时长：${value[3] ? value[3] : ""}`;
-      },
-    },
-    xAxis: {
-      type: "category",
-    },
-    // 数组形式,左右连个纵坐标，同时series也要有多个
-    yAxis: [
-      {
-        type: "value",
-        name: "时间点",
-      },
-      {
-        type: "value",
-        name: "时长/h",
-      },
-    ],
-    dataset: {
-      source: [
-        ["7/5", "6.12", "20.12", 7],
-        ["7/6", "7.12", "20.12", 5],
-        ["7/7", "7.12", "22.12"],
-        ["7/8", "7.12", "", 6],
-        ["7/9", "7.12", "19.12", 4],
-        ["7/10", "7.12", "20.12", 10],
-        ["7/11", "7.12", "21.12", 9],
-        ["7/12", "6.12", "20.12", 7],
-        ["7/13", "7.12", "20.12", 5],
-        ["7/14", "7.12", "22.12"],
-        ["7/15", "7.12", "", 6],
-        ["7/16", "7.12", "19.12", 4],
-        ["7/17", "7.12", "20.12", 10],
-        ["7/18", "7.12", "21.12", 9],
-        ["7/19", "7.12", "21.12", 9],
-        ["7/20", "7.12", "21.12", 9],
-        ["7/21", "7.12", "21.12", 9],
-        ["7/22", "7.12", "21.12", 9],
-        ["7/23", "7.12", "21.12", 9],
-        ["7/24", "7.12", "21.12", 9],
-        ["7/25", "7.12", "21.12", 9],
-        ["7/26", "7.12", "21.12", 9],
-        ["7/27", "7.12", "21.12", 9],
-        ["7/28", "7.12", "21.12", 9],
-        ["7/29", "7.12", "21.12", 9],
-        ["7/30", "7.12", "21.12", 9],
-        ["7/31", "7.12", "21.12", 9],
-      ],
-    },
-    // 单个或者多个组合
-    series: [
-      {
-        type: "bar",
-        encode: {
-          x: 0,
-          y: 3,
-        },
-        yAxisIndex: 1,
-        itemStyle: {
-          normal: {
-            color: "#2F80ED",
-          },
-        },
-        emphasis: {
-          itemStyle: {
-            // 高亮时点的颜色。
-            color: "#06A8F5",
-          },
-          // label: {
-          //   show: true,
-          //   // 高亮时标签的文字。
-          //   // formatter: "This is a emphasis label.",
-          // },
-        },
-      },
-      {
-        type: "line",
-        encode: {
-          x: 0,
-          y: 1,
-        },
-        itemStyle: {
-          normal: {
-            color: "#00b09b",
-          },
-        },
-        yAxisIndex: 0,
-      },
-      {
-        type: "line",
-        encode: {
-          x: 0,
-          y: 2,
-        },
-        itemStyle: {
-          normal: {
-            color: "#C06C84",
-          },
-        },
-        yAxisIndex: 0,
-      },
-    ],
-  };
-
-  chart.setOption(option);
-  return chart;
+  chart.setOption(options);
 }
 
 Page({
@@ -263,16 +136,24 @@ Page({
       fail: function () {},
     };
   },
+
+  onReady() {
+    // 获取组件
+    this.ecWeekComponent = this.selectComponent("#mychart-week-bar");
+    this.ecMonthComponent = this.selectComponent("#mychart-month-bar");
+  },
+
   data: {
     ecWeek: {
-      onInit: initChart,
+      lazyLoad: true,
+    },
+    ecMonth: {
+      lazyLoad: true,
     },
     ecMonth: {
       onInit: initMonthChart,
     },
   },
-
-  onReady() {},
 
   onload() {
     const userId = wx.getStorageSync("userId");
@@ -288,14 +169,74 @@ Page({
       success(res) {
         console.log(res.data);
         const {
-          dayList,
-          averageSleepTime,
-          averageWakeUpTime,
-          averageBedTime,
+          weekDaysList,
+          monthDaysList,
+          weekSleepTime,
+          weekWakeUpTime,
+          weekBedTime,
+          monthSleepTime,
+          monthWakeUpTime,
+          monthBedTime,
+          totalAverageSleepTime,
+          totalAverageWakeUpTime,
+          totalAverageBedTime,
         } = res.data;
+        const rData = [
+          {
+            id: 7,
+            dateTime: "2020-08-10",
+            sleepTime: "8.48",
+            wakeUpTime: "05:12:19",
+            bedTime: "22:15:19",
+          },
+          {
+            id: 8,
+            dateTime: "2020-08-11",
+            sleepTime: "8.28",
+            wakeUpTime: "06:45:12",
+            bedTime: "23:05:55",
+          },
+          {
+            id: 9,
+            dateTime: "2020-08-12",
+            sleepTime: "",
+            wakeUpTime: "07:23:12",
+            bedTime: "22:45:12",
+          },
+        ];
         // 处理数据
         // setState
+        this.ecWeekComponent.init((canvas, width, height, dpr) => {
+          // 获取组件的 canvas、width、height 后的回调函数
+          // 在这里初始化图表
+          const chart = echarts.init(canvas, null, {
+            width: width,
+            height: height,
+            devicePixelRatio: dpr, // new
+          });
+
+          setOption(chart, 'week', rData);
+
+          // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+          // this.chart = chart;
+
+          // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+          return chart;
+        });
+
+        this.ecMonthComponent.init((canvas, width, height, dpr) => {
+          const chart = echarts.init(canvas, null, {
+            width: width,
+            height: height,
+            devicePixelRatio: dpr, // new
+          });
+          setOption(chart, 'month', rData);
+          return chart;
+        });
       },
+      fail() {
+        
+      }
     });
   },
 });
