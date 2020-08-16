eda8ac966f127cf68d22517ce73250d407f586cd
xblcity <hyaxie@163.com>
2020-08-16 23:34:16 +0800

    feat: 数据延迟展示与数据处理

--- pages/record/index.js
+++ pages/record/index.js
@@ -1,4 +1,5 @@
 import * as echarts from "../../ec-canvas/echarts";
+import moment from '../../utils/moment.min.js';
 
 const app = getApp();
 
@@ -6,8 +7,8 @@ const typeMap = {
   week: "本周统计",
   month: "本月统计",
 };
+
 function setOption(chart, type, data) {
-  let type = "week";
   let dayTotal;
   if (type === "week") {
     dayTotal = 7;
@@ -18,17 +19,21 @@ function setOption(chart, type, data) {
   const source = [...new Array(dayTotal)]
     .map((item, index) =>
       type === "week"
-        ? moment().startOf("week").add(index, "days").format("YYYY-MM-DD")
+        ? moment().startOf("isoWeek").add(index, "days").format("YYYY-MM-DD")
         : moment().startOf("month").add(index, "days").format("YYYY-MM-DD")
     )
     .map((date) => {
+      console.log(date, data)
       const target = data.find((item) => item.dateTime === date);
+      const newDate = moment(date).format('MM/DD')
       if (target) {
-        const { dateTime, sleepTime, wakeUpTime, bedTime } = target;
-        return [dateTime, sleepTime, wakeUpTime, bedTime];
+        const { sleepTime, wakeUpTime, bedTime } = target;
+        console.log(sleepTime, wakeUpTime, bedTime, 999)
+        return [newDate, wakeUpTime, bedTime, Number(sleepTime)];
       }
-      return [date, "", "", "", ""];
+      return [newDate, "", "", "", ""];
     });
+  console.log(source, 88888)
   const options = {
     // 标题
     title: {
@@ -48,7 +53,8 @@ function setOption(chart, type, data) {
         // 时间转换
         const wakeUpTime = value[1] && value[1].split();
         const bedTime = value[2] && value[2].replace(".", ":");
-        return `  起床时间：${wakeUpTime}
+        return `  日期: ${value[0]}
+  起床时间：${wakeUpTime}
   睡觉时间：${bedTime}
   睡眠时长：${value[3] ? value[3] : ""}`;
       },
@@ -68,7 +74,16 @@ function setOption(chart, type, data) {
       },
     ],
     dataset: {
-      source,
+      // source: [
+      //   ["7/12", "6.12", "20.12", 7],
+      //   ["7/13", "7.12", "20.12", 5],
+      //   ["7/14", "7.12", "22.12"],
+      //   ["7/15", "7.12", "", 6],
+      //   ["7/16", "7.12", "19.12", 4],
+      //   ["7/17", "7.12", "20.12", 10],
+      //   ["7/18", "7.12", "21.12", 9],
+      // ],
+      source
     },
     // 单个或者多个组合
     series: [
@@ -137,12 +152,6 @@ Page({
     };
   },
 
-  onReady() {
-    // 获取组件
-    this.ecWeekComponent = this.selectComponent("#mychart-week-bar");
-    this.ecMonthComponent = this.selectComponent("#mychart-month-bar");
-  },
-
   data: {
     ecWeek: {
       lazyLoad: true,
@@ -150,19 +159,66 @@ Page({
     ecMonth: {
       lazyLoad: true,
     },
-    ecMonth: {
-      onInit: initMonthChart,
-    },
   },
 
-  onload() {
-    const userId = wx.getStorageSync("userId");
-    // 用户不存在或者未登录，跳转至mine
-    // if (!userId) {
-    //   wx.switchTab({
-    //     url: "../mine/index",
-    //   });
-    // }
+  onReady() {
+    // 获取组件
+    this.ecWeekComponent = this.selectComponent("#mychart-week-line");
+    this.ecMonthComponent = this.selectComponent("#mychart-month-line");
+
+    // 数据 处理 成 05:18:19 -> 5.18
+    const rData = [
+      {
+        id: 7,
+        dateTime: "2020-08-11",
+        wakeUpTime: "5.12",
+        bedTime: "22.15",
+        sleepTime: "8.48",
+      },
+      {
+        id: 8,
+        dateTime: "2020-08-12",
+        sleepTime: "8.28",
+        wakeUpTime: "6.45",
+        bedTime: "23.05",
+      },
+      // {
+      //   id: 9,
+      //   dateTime: "2020-08-15",
+      //   sleepTime: "",
+      //   wakeUpTime: "07:23:12",
+      //   bedTime: "22:45:12",
+      // },
+    ];
+    // 处理数据
+    // setState
+    this.ecWeekComponent.init((canvas, width, height, dpr) => {
+      // 获取组件的 canvas、width、height 后的回调函数
+      // 在这里初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr, // new
+      });
+
+      setOption(chart, 'week', rData);
+
+      // 将图表实例绑定到 this 上，可以在其他成员函数（如 dispose）中访问
+      // this.chart = chart;
+
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return chart;
+    });
+
+    this.ecMonthComponent.init((canvas, width, height, dpr) => {
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr, // new
+      });
+      setOption(chart, 'month', rData);
+      return chart;
+    });
 
     wx.request({
       url: `${app.globalData.host}/postDayList`,
@@ -235,8 +291,19 @@ Page({
         });
       },
       fail() {
-        
       }
     });
+  },
+
+  onLoad() {
+    // const userId = wx.getStorageSync("userId");
+    // 用户不存在或者未登录，跳转至mine
+    // if (!userId) {
+    //   wx.switchTab({
+    //     url: "../mine/index",
+    //   });
+    // }
+
+    
   },
 });
